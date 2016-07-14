a7c7bc4e6dd9182c39a55fa5226f3f966ba6de8c
rameshho <rameshh035@gmail.com>
2016-07-14 17:09:38 +0530

    Add files via upload

---
+++ recursive_and_decorative.py
@@ -0,0 +1,20 @@
+__author__ = 'rameshho'
+
+def check_datatype ( reverse_string s):
+    def my_reverse_string(string):
+        if type(string) == str:
+            return reverse_string(string)
+        else:
+            print("It is of not type string")
+            return False
+    return my_reverse_string
+
+@check_datatype
+def reverse_string(string):
+
+    if string == "":
+        return string
+    else:
+        return reverse_string(string[1:]) + string[0]
+
+print(reverse_string("ramesh"))
